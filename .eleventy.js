f9831e397d8fd93d242873670bd411ec663e9db6
batmanwgd <batmanwgd@gmail.com>
2020-05-06 12:46:29 -0400

    extra tidlybits

--- .eleventy.js
+++ .eleventy.js
@@ -33,7 +33,7 @@ module.exports = function(eleventyConfig) {
     return array.slice(0, n);
   });
 
-  //eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
